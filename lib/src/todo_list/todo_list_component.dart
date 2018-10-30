471cd8d9b8d763fdac8a04db0f1ce11d4c7ffd99
zkx1022 <1255220209@qq.com>
2018-10-30 23:43:01 +0800

    第一次修改“

--- lib/src/todo_list/todo_list_component.dart
+++ lib/src/todo_list/todo_list_component.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
-
+import 'dart:math';
+import 'dart:html';
 import 'package:angular/angular.dart';
 import 'package:angular_components/angular_components.dart';
 
@@ -35,7 +36,11 @@ class TodoListComponent implements OnInit {
   void add() {
     items.add(newTodo);
     newTodo = '';
+    var random=Random();
+  var number=random.nextInt(13);
+   var lists=["陈瑶","朱子恒","周嘉翔","唐莉雯","张静雅","龙晶毅","朱鹏伟","戚晓颖","郑可欣","李典康","吴松二","蔡心蕊","赵世宇"];
+   querySelector('#tips').text=lists[number];
   }
 
-  String remove(int index) => items.removeAt(index);
+  /*String remove(int index) => items.removeAt(index);*/
 }
